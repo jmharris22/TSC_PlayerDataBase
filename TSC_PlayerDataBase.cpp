693b436dac0a9d604b1046dc2b3099a35ee541fb
Justin Harris <jharris.cds@gmail.com>
2015-06-05 14:04:29 -0700

    Success: db connection

--- TSC_PlayerDataBase.cpp
+++ TSC_PlayerDataBase.cpp
@@ -7,8 +7,13 @@
 #include <thread>
 #include <mutex>
 #include <atomic>
+#include <stdio.h>
+#include "sqlite3.h"
 
 using namespace std;
+
+string processInput(string input);
+sqlite3_stmt * queryDaabase(string query);
 
 struct Data
 {
@@ -19,6 +24,8 @@ struct Data
 
 unordered_map<long int, Data> tickets;
 mutex mtx;
+
+sqlite3 *dataBase;
 
 atomic<bool> worker_working(false);
 long int id = 0; // global ticket id
@@ -38,7 +45,7 @@ void worker()
 		mtx.unlock();
 
 		string input = ticket.params; // get input
-		string output = "output: " + input; // process input
+		string output = processInput(input); // process input
 		ticket.result = output; // prepare result
 		ticket.ready = true; // notify about result
 
@@ -49,79 +56,160 @@ void worker()
 }
 
 
+string executeGetterQueryInt(string query){
+	sqlite3_stmt *res = queryDaabase(query);
+	int result = 0;
+	while (sqlite3_step(res) == SQLITE_ROW){
+		result = (sqlite3_column_int(res, 0));
+	}
+	
+	return std::to_string(result);;
+}
+
+string executeGetterQueryString(string query){
+	sqlite3_stmt *res = queryDaabase(query);
+	string result = "";
+	while (sqlite3_step(res) == SQLITE_ROW){
+		result = std::string(reinterpret_cast<const char*>(sqlite3_column_text(res, 0)));
+	}
+	
+	return result;
+}
+
+sqlite3_stmt * queryDaabase(string query) {
+	sqlite3_stmt *res;
+	int rec_count = 0;
+	const char *errMSG;
+	const char *tail;
+
+	int error = sqlite3_open("tsc_database.db", &dataBase);
+	if (error)
+	{
+		//
+	}
+
+	error = sqlite3_prepare_v2(dataBase,
+		query.c_str(),
+		1000, &res, &tail);
+
+	if (error != SQLITE_OK)
+	{
+		//
+	}
+
+	return res;
+}
 
 string getPlayerMoney(string playerUID) {
-
+	string query = "SELECT Money FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryInt(query);
+	return output;
 }
 
 string getPlayerUniform(string playerUID) {
-
+	string query = "SELECT Uniform FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerVest(string playerUID) {
-
+	string query = "SELECT Vest FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerHeadgear(string playerUID) {
-
+	string query = "SELECT Headgear FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerPrimaryWeapon(string playerUID) {
-
+	string query = "SELECT PrimaryWeapon FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerSecondaryWeapon(string playerUID) {
-
+	string query = "SELECT SecondaryWeapon FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerMagazines(string playerUID) {
-
+	string query = "SELECT Magazines FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerBackpack(string playerUID) {
-
+	string query = "SELECT Backpack FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerUniformItems(string playerUID) {
-
+	string query = "SELECT UniformItems FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerVestItems(string playerUID) {
-
+	string query = "SELECT VestItems FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 string getPlayerBackpackItems(string playerUID) {
-
+	string query = "SELECT BackpackItems FROM Players WHERE Uid=" + playerUID;
+	string output = executeGetterQueryString(query);
+	return output;
 }
 
 bool isPlayerInDatabase(string playerUID) {
 
 }
 
-void processInput(string input) {
-	string command = input.substr(0, 1);
-	string output = "EMPTY";
+string processInput(string input) {
+	string command = input.substr(2, 1);
+	string output = command;
+	string inputClean = input.substr(3);
+
 	if (!command.compare("A")) {
-		output = getPlayerMoney(input.substr(2));
+		output = getPlayerMoney(inputClean);
 	}
 	else if (!command.compare("B")) {
-
+		output = getPlayerUniform(inputClean);
 	}
 	else if (!command.compare("C")) {
-
+		output = getPlayerVest(inputClean);
 	}
 	else if (!command.compare("D")) {
-
+		output = getPlayerHeadgear(inputClean);
 	}
 	else if (!command.compare("E")) {
-
+		output = getPlayerPrimaryWeapon(inputClean);
 	}
 	else if (!command.compare("F")) {
-
+		output = getPlayerSecondaryWeapon(inputClean);
 	}
 	else if (!command.compare("G")) {
-
+		output = getPlayerMagazines(inputClean);
 	}
+	else if (!command.compare("H")) {
+		output = getPlayerBackpack(inputClean);
+	}
+	else if (!command.compare("I")) {
+		output = getPlayerUniformItems(inputClean);
+	}
+	else if (!command.compare("J")) {
+		output = getPlayerVestItems(inputClean);
+	}
+	else if (!command.compare("K")) {
+		output = getPlayerBackpackItems(inputClean);
+	}
+
+	return output;
 }
 
 
@@ -170,4 +258,3 @@ void __stdcall RVExtension(char *output, int outputSize, const char *function)
 		strncpy_s(output, outputSize, "INVALID COMMAND", _TRUNCATE); // other input
 	}
 }
-
